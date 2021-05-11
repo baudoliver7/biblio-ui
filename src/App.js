93d548be2aefdc0580879684d522bcaa28cfea1e
Olivier B. OURA <baudolivier.oura@gmail.com>
2021-05-11 15:46:20 +0000

    Liste les livres #1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    isLoading: true,
+    groups: []
+  };
+
+  async componentDidMount() {
+    const response = await fetch('/Livres');
+    const body = await response.json();
+    this.setState({ livres: body, isLoading: false });
+  }
+
+  render() {
+    const {livres, isLoading} = this.state;
+
+    if (isLoading) {
+      return <p>Loading...</p>;
+    }
+
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <div className="App-intro">
+            <h2>Liste de livres</h2>
+            {livres.map(livre =>
+              <div key={livre.id}>
+                {livre.intitule}
+              </div>
+            )}
+          </div>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
